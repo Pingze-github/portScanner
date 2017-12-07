0ecc127dad145eecb80f317ca764357ba4c74fde
wangshunzhang <wangshunzhang@henhaoji.com>
2017-12-07 10:23:23 +0800

    增加参数调用，修缮输出，补充README

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 # portScanner
 高效率端口扫描器。Go语言编写。
 
+### 编译
+```go run main.go```
+#### 交叉编译
+Linux x64:
+```
+SET CGO_ENABLED=0
+SET GOOS=linux
+SET GOARCH=amd64
+go build main.go
+```
+Windows x64:
+```
+SET CGO_ENABLED=0
+SET GOOS=windows
+SET GOARCH=amd64
+go build main.go
+```
+
+### 使用
+编译为 main 后，
+
+```./main -ip 127.0.0.1 -port 1-65535 -max 10000```
+
+> port 有三种指定方式：
+> 1. `100` 指定单个
+> 2. `100,101,102` 指定多个
+> 3. `1-100` 指定范围
+
+输出所有开端口（tcp）：
+
+```21,80,111,3166```
+
 ### 测试
 
 条件：并发数: 10000; 超时时间: 3s; 扫描范围: 1-65535
--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"runtime"
 	"strconv"
 	"time"
+	"flag"
+	"regexp"
+	"strings"
 )
 
 func scanRange(ip string, portRange [2]int, gomax int, timeout int) []int {
@@ -19,7 +22,7 @@ func scanRange(ip string, portRange [2]int, gomax int, timeout int) []int {
 		address := ip + ":" + strconv.Itoa(port)
 		_, err := net.DialTimeout("tcp", address, time.Duration(timeout))
 		if err != nil {
-			fmt.Println(err)
+			//fmt.Println(err)
 		} else {
 			openPorts = append(openPorts, port)
 		}
@@ -41,14 +44,72 @@ func scanRange(ip string, portRange [2]int, gomax int, timeout int) []int {
 	return openPorts
 }
 
-func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	ip := "127.0.0.1"
-	var portRange = [2]int{1, 65535}
-	gomax := 10000
-	start := time.Now().UnixNano()
-	openPorts := scanRange(ip, portRange, gomax, 3e9)
-	fmt.Println(openPorts)
-	fmt.Println((time.Now().UnixNano() - start) / 1e6)
+func join(intArr []int) string {
+	str := ""
+	for i := 0; i < len(intArr); i++ {
+		str += strconv.Itoa(intArr[i])
+		if i != len(intArr) - 1 {
+			str += ","
+		}
+	}
+	return str
 }
+
+var (
+	ip string
+	port string
+	max int
+	portRange [2]int
+)
+
+func init() {
+	flag.StringVar(&ip, "ip", "", "IP to detect")
+	flag.StringVar(&port, "port", "", "Ports to detect")
+	flag.IntVar(&max, "max", 5000, "Max concurrent number")
+	flag.Parse()
+	match1, _ := regexp.MatchString("^\\d+-\\d+$", port)
+	match2 := strings.Contains(port, ",")
+	if match1 {
+		index := strings.Index(port, "-")
+		start, err := strconv.Atoi(string([]rune(port)[:index]))
+		if err != nil {
+			fmt.Println("Error: param port")
+			return
+		}
+		end, err := strconv.Atoi(string([]rune(port)[index+1:]))
+		if err != nil {
+			fmt.Println("Error: param port")
+			return
+		}
+		portRange = [2]int{start, end}
+	} else if match2 {
+		splits := strings.Split(port, ",")
+		if len(splits) < 2 {
+			fmt.Println("Error: param port")
+			return
+		}
+		start, _ := strconv.Atoi(splits[0])
+		end, _ := strconv.Atoi(splits[1])
+		// TODO 这种输入格式时，检测输入的几个端口，而不是范围
+		portRange = [2]int{start, end}
+	} else {
+		start, err := strconv.Atoi(port)
+		if err != nil {
+			fmt.Println("Error: param port")
+			return
+		}
+		end, _ := strconv.Atoi(port)
+		portRange = [2]int{start, end}
+	}
+}
+
+func main() {
+	//fmt.Println(ip, portRange, max)
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	//start := time.Now().UnixNano()
+	openPorts := scanRange(ip, portRange, max, 3e9)
+	fmt.Println(join(openPorts))
+	//fmt.Println((time.Now().UnixNano() - start) / 1e6, "ms")
+}
+
 
